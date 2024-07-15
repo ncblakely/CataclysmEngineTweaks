5da194028a72c1151939ef71d92d68bd2268227c
Nick Blakely <ncblakely@gmail.com>
2024-07-14 23:41:37 -0700

    Define remaining groups.

--- game/Game/AISkirmish.h
+++ game/Game/AISkirmish.h
@@ -2,18 +2,18 @@
 
 // Reverse engineered structures for Cataclysm's new skirmish AI
 
-#define NUM_AIS_TEAMS 34
+constexpr int NUM_AIS_TEAMS = 34;
 
 struct AISTeam
 {
 	MaxSelection sel[10];
-	ubyte unk[0x78];
-	udword teamsize; // Seems team size related, but differs from totalteamsize.
+	ubyte unk1[0x78];
 };
 
 struct AISTeamEntry
 {
 	AISTeam team;
+	udword numselections;
 	udword totalteamsize;
 };
 
@@ -21,33 +21,116 @@ namespace AISTeamType
 {
 	enum
 	{
+		// sRecon
+		// bRecon
 		Recon = 0,
 		ReconEnemy = 1,
+
+		// sAcolyte
+		// sACV
+		// bAcolyte
+		// bInterceptor
+		// bCloakedFighter
+		// bAttackBomber
+		// bACV
 		Interceptor = 2,
 		InterceptorEnemy = 3,
+
+		// sMultiBeamFrigate
+		// bMultiGunCorvette
+		// bMultiBeamFrigate
+		AntiFighter = 4, // Multibeam, multigun corvette
+		AntiFighterEnemy = 5,
+
+		// sSentinel
+		// bSentinel
 		Sentinel = 6,
+		SentinelEnemy = 7,
+
+		// sRammingFrigate
+		// sHiveFrigate
+		// sDreadnought
+		// sDestroyer
+		// bHeavyCorvette
+		// bHiveFrigate
+		// bIonArrayFrigate
+		// bHeavyCruiser
+		// bRammingFrigate
+		// bCruiseMissile
+		// bMissileCorvette
+		MediumCombat = 8,
+		MediumCombatEnemy = 9,
+
 		Main = 10,
+
+        // sMothershipSpecial
+		// sMothershipEngineering
+		// sMothershipArmour
+		// sMothershipMicro
+		// sMothershipWeapons
+		// sMothershipDockingBay
+		// sMothershipLowerSpine
+		// sMothershipBigGun
 		MothershipModules = 11,
+
+		// sCarrierSupport
 		CarrierSupport1 = 12,
 		CarrierSupport2 = 13,
+
+		// sMothershipSupport
+		// bMothershipSupport
 		MothershipSupport = 14,
+		MothershipSupportEnemy = 16,
+
+		// sMothershipBigGun
+		MothershipBigGun = 15,
+
+		// sCarrier
+		// bCarrier
 		Carrier = 17,
+		CarrierEnemy = 18,
+
+		// sMothership
+		// bMothership
 		Mothership = 19,
 		MothershipEnemy = 20,
+
+		// sProcessor
+		// bProcessor
 		Processor = 21,
+		ProcessorEnemy = 22,
+
+		// sMimic
+		// sMCV
 		Mimic = 23,
+		MimicEnemy1 = 24, // sub_53F9F0 determines which enemy group is assigned
+		MimicEnemy2 = 25,
+
 		CruiseMissile = 26,
+
+		// sLeech
 		Leech = 27,
 		LeechEnemy = 28,
+
+		// bDFGFrigate
 		DFGFrigate = 29,
 		DFGFrigateEnemy = 30,
+
+		// sWorker
+		// bWorker
 		Worker = 31,
+		WorkerEnemy = 32,
+
+		// dCrystal0
+		// dCrystal1
 		CrystalSalvage = 33,
 	};
 }
 
-static_assert(offsetof(AISTeamEntry, team.teamsize) == 0x9D08);
+static_assert(offsetof(AISTeamEntry, numselections) == 0x9D08);
 static_assert(offsetof(AISTeamEntry, totalteamsize) == 0x9D0C);
 static_assert(sizeof(AISTeamEntry) == 0x9D10);
+static_assert(sizeof(AISTeamEntry)* NUM_AIS_TEAMS == (0xA2D980 - 0x008DFD60), "AI team info overflow");
 
+void DumpAITeams();
 void aisFleetUpdate();
