fffc538bcaaf285b06c519de8079236251da6637
Nick Blakely <ncblakely@gmail.com>
2024-07-22 22:21:04 -0700

    Add release packaging script.

---
+++ Compress-Release.ps1
@@ -0,0 +1,17 @@
+
+$hwcRoot = $env:HWC_Root
+$cetRoot = $env:CET_Root
+
+$files = @(
+    (Join-Path $hwcRoot "cataclysm_loader.exe"), 
+    (Join-Path $hwcRoot "CataclysmEngineTweaks.dll"), 
+    (Join-Path $cetRoot "CataclysmEngineTweaks.ini")
+    (Join-Path $hwcRoot "rgl.dll"), 
+    (Join-Path $hwcRoot "rgld3d.dll")) 
+
+$compress = @{
+    LiteralPath      = $files
+    CompressionLevel = "Optimal"
+    DestinationPath  = (Join-Path $hwcRoot "cet.zip")
+}
+Compress-Archive @compress
